4c26c3f47cbfd15a1ddaffab5c5d004bd3c6b527
Szymon Strzałkowski <109031803+SzymonStrzalkowski@users.noreply.github.com>
2023-09-10 22:10:10 +0200

    New vision using a plain C lense.
    After coming back after a year to this program and seeing the C++ i've wrote, well i'm disgusted by it.
    I'll remake the program using plain C and build out functionality to use it as a day-planer maybe in the future easily being integrable with suckless dwm.

---
+++ calendar.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+//struct {
+//string line[5]={"|Su|Mo|Tu|We|Th|Fr|Sa|","+--+--+--+--+--+--+--+","|  ","| ","|"};
+//}calendartop;
+
+int leap_year_check(int febuary,int** year){
+	if((**year%4==0&&**year%400==0&&**year%3200!=0)||(**year%4==0&&**year%100!=0))
+		return febuary+1;
+	return febuary;
+}
+
+int first_day_of_the_year(int** year){
+	int x=(**year-1)/100;
+	int y=(**year-1)%100;
+	return (((29-(2*x)+y+(y/4)+(x/4))%7)+7)%7;
+}
+
+void printcal(int* year,int *month){
+	char day[7][2]={"Su","Mo","Tu","We","Th","Fr","Sa"};
+	int months[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+	months[1]=leap_year_check(months[1],&year);
+	int first_day=first_day_of_the_year(&year);
+	printf("%d\n",first_day);
+	for(int i=0;i!=*month-1;i++){
+		first_day+=months[i];
+		first_day%=7;
+	}
+	switch(first_day){
+		case 0:
+			break;
+		default:
+			for(int i=0;i<first_day;i++){
+				printf("|%c%c",day[i][0],day[i][1]);
+			}
+	}
+
+}
+
+int main(int argc, char *argv[]){
+	int year,month;
+	printf("What year:");
+	scanf("%d", &year);
+	printf("What month:");
+	scanf("%d", &month);
+	printf("The year passed into is %d, and the month is %d. \n", year, month);
+	printcal(&year,&month);
+	return 0;
+}
